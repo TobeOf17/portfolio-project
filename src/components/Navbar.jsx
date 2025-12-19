b50aa5d2aaa61a9cc111e03731939680f87d32f0
Tobechukwu Ofili <ofilitobe@gmail.com>
2025-12-20 00:47:46 +0100

    create App and Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-content">
+          <Link to="/" className="navbar-logo">Your Name</Link>
+          <ul className="navbar-menu">
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul> 
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
